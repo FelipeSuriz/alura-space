b6b09d4c0db5747eb1fe1182eaa58263ad071285
Felipe Suriz <felipesuriz@hotmail.com>
2023-07-21 13:15:28 -0300

    componente cards

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -1,36 +1,14 @@
-import React from 'react'
-import home from '../../assets/icones/home.png'
-import maisCurtidas from '../../assets/icones/mais-curtidas.png'
-import maisVistas from '../../assets/icones/mais-vistas.png'
-import novas from '../../assets/icones/novas.png'
-import surpreendaMe from '../../assets/icones/surpreenda-me.png'
-import styles from './Menu.module.scss'
+import React from 'react';
+import style from './Menu.module.scss';
+import icones from './icones.json';
+import Icone from './Icone';
 
 export default function Menu() {
   return (
-    <nav className={styles.menu}>
-        <ul className={styles.menu__lista}>
-            <li className={styles.menu__item}>
-                <img src={home} alt="ícone do home" />
-                <a href="/">Início</a>
-            </li>
-            <li className={styles.menu__item}>
-                <img src={maisCurtidas} alt="ícone das mais curtidas" />
-                <a href="/">Mais curtidas</a>
-            </li>
-            <li className={styles.menu__item}>
-                <img src={maisVistas} alt="ícone das mais vistas" />
-                <a href="/">Mais vistas</a>
-            </li>
-            <li className={styles.menu__item}>
-                <img src={novas} alt="ícone das novas" />
-                <a href="/">Novas</a>
-            </li>
-            <li className={styles.menu__item}>
-                <img src={surpreendaMe} alt="ícone do surpreenda-me" />
-                <a href="/">Surpreenda-me</a>
-            </li>
-        </ul>
+    <nav className={style.menu}>
+      <ul className={style.menu__lista}>
+        {icones.map(icone => (<Icone key={icone.id} icone={icone} style={style} />))}
+      </ul>
     </nav>
   )
 }
